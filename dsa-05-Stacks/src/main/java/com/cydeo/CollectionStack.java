9dd7d846533bdbbf94220c6721403ee21a4fcf8b
mizgin <mizgincvk@gmail.com>
2022-09-24 22:53:01 -0400

    Deque

---
+++ dsa-05-Stacks/src/main/java/com/cydeo/CollectionStack.java
@@ -0,0 +1,19 @@
+package com.cydeo;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class CollectionStack {
+
+    public static void main(String[] args) {
+
+        Deque<Integer> cStack = new LinkedList<>();
+        cStack.push(1); //add first
+        cStack.push(2);
+        cStack.push(3);
+        System.out.println(cStack.peek()); //get first
+        System.out.println(cStack.pop()); //remove and print first
+        System.out.println("peek is: " + cStack.peek()); //3 is removed from top
+
+    }
+}
