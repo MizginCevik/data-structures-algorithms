674c1b6cada4484ed1663ab7054d752b7b7df35b
mizgin <mizgincvk@gmail.com>
2022-12-22 14:48:56 -0500

    Insertion Sorting

--- dsa-11-Sorting/src/main/java/SortingMain.java
+++ dsa-11-Sorting/src/main/java/SortingMain.java
@@ -5,9 +5,12 @@ public class SortingMain {
     public static void main(String[] args) {
 
         int[] numbers = {7,2,4,6,1,9,-1};
+        int[] nums = {2,3,4,1};
 
 //        System.out.println(Arrays.toString(BubbleSort.bubbleSort(numbers)));
-        System.out.println(Arrays.toString(SelectionSort.selectionSort(numbers)));
+//        System.out.println(Arrays.toString(SelectionSort.selectionSort(numbers)));
+        System.out.println(Arrays.toString(InsertionSort.insertionSort(numbers)));
+        System.out.println(Arrays.toString(InsertionSort.insertionSort(nums)));
 
     }
 
