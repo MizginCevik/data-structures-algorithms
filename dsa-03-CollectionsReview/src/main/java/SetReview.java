939cbada3a46e1616ffb3d4333df2b92e681adde
mizgin <mizgincvk@gmail.com>
2022-12-25 23:59:00 -0500

    compared HashSet and LinkedHashSet, and solution of findFirstRepeatingCharacter

--- dsa-03-CollectionsReview/src/main/java/SetReview.java
+++ dsa-03-CollectionsReview/src/main/java/SetReview.java
@@ -6,35 +6,42 @@ public class SetReview {
 
     public static void main(String[] args) {
         //1. create a set
-        Set<Student> set = new LinkedHashSet<>();
-        // 2 add element
+        Set<Student> set = new HashSet<>();
 
+        //2. add element
         set.add(new Student(1, "Jack"));
         set.add(new Student(2, "Julia"));
         set.add(new Student(4, "Mary"));
         set.add(new Student(3, "Mike"));
-        set.add(new Student(4, "Mary"));
-
+        set.add(new Student(4, "Mary")); // duplicates are not allowed
 
         System.out.println(set);
-        //String st= "Javva Developer";
 
-        //System.out.println(firstRepeatingChar(st));
+        Set<Student> linkedSet = new LinkedHashSet<>(); // keeps the insertion order
+        linkedSet.add(new Student(1, "Jack"));
+        linkedSet.add(new Student(2, "Julia"));
+        linkedSet.add(new Student(4, "Mary"));
+        linkedSet.add(new Student(3, "Mike"));
+        linkedSet.add(new Student(4, "Susan"));
+
+        System.out.println(linkedSet);
+
+        // Task:
+        String st = "Java Developer";
+        System.out.println(firstRepeatingChar(st));
 
     }
 
-    public static Character firstRepeatingChar(String str) {
-        // What is the time complexity of the following code? O(n)
-
-
+    public static Character firstRepeatingChar(String str) { // time complexity O(n)
         // create a hashSet
         Set<Character> chars = new HashSet<>(); // in here I have Space Complexity of O(n)
+
         // iterate over the char array and add chars into hashSet
-
-
-        for (Character ch : str.toCharArray()) if (!chars.add(ch)) return ch;
-        // if add ops is false return that char
-
+        for (Character ch : str.toCharArray()) {
+            if (!chars.add(ch)) { // if add ops is false return that char
+                return ch;
+            }
+        }
         return null;
     }
 
